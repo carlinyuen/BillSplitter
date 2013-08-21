3ae5803589d4903b07984e5192f23910856b885c
Carlin <email.me@carlinyuen.com>
2013-08-21 15:42:57 -0400

    adding views

---
+++ BillSplitter/iOSProjectTemplate/Views & Controllers/BSTotalMarkupViewController.h
@@ -0,0 +1,14 @@
+/**
+	@file	BSTotalMarkupViewController.h
+	@author	Carlin
+	@date	8/21/13
+	@brief	BillSplitter
+*/
+//  Copyright (c) 2013 Carlin. All rights reserved.
+
+
+#import <UIKit/UIKit.h>
+
+@interface BSTotalMarkupViewController : UIViewController
+
+@end
