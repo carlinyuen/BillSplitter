7a730172f428fec54944bc942b0a13e478af682e
Carlin <email.me@carlinyuen.com>
2013-12-16 21:49:34 -0500

    more layouts

--- BillSplitter/iOSProjectTemplate/Views & Controllers/BSTotalMarkupViewController.m
+++ BillSplitter/iOSProjectTemplate/Views & Controllers/BSTotalMarkupViewController.m
@@ -266,6 +266,7 @@
     );
     
     frame = self.finalDivider.frame;
+    frame.origin.y = CGRectGetMaxY(frame) + UI_SIZE_MARGIN;
     frame.size.height = bounds.size.height / 5;
     self.finalLabel = [[UILabel alloc] initWithFrame:frame];
     self.finalLabel.text = @"$0.00";
