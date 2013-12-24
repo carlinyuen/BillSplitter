a63f05e9391f9f56e0f9937262c19688c2362aed
Carlin <email.me@carlinyuen.com>
2013-12-24 01:27:44 -0800

    Adding iRate into app

---
+++ BillSplitter/iOSProjectTemplate/Views & Controllers/BSFeedbackViewController.h
@@ -0,0 +1,13 @@
+//
+//  BSFeedbackViewController.h
+//  BillSplitter
+//
+//  Created by . Carlin on 12/24/13.
+//  Copyright (c) 2013 Carlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BSFeedbackViewController : UIViewController
+
+@end
---
+++ BillSplitter/iOSProjectTemplate/Views & Controllers/BSFeedbackViewController.m
@@ -0,0 +1,38 @@
+//
+//  BSFeedbackViewController.m
+//  BillSplitter
+//
+//  Created by . Carlin on 12/24/13.
+//  Copyright (c) 2013 Carlin. All rights reserved.
+//
+
+#import "BSFeedbackViewController.h"
+
+@interface BSFeedbackViewController ()
+
+@end
+
+@implementation BSFeedbackViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
